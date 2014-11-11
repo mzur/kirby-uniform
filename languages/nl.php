e21f4de47003429033ddbbb7db5909e1ab43ebe0
Jöran Aerns <joranovski@gmail.com>
2014-11-11 21:13:35 +0100

    Added dutch translation
    Signed-off-by: Jöran Aerns <joranovski@gmail.com>

---
+++ languages/nl.php
@@ -0,0 +1,7 @@
+<?php
+
+l::set('sendform-default-subject', 'Bericht van het webformulier');
+l::set('sendform-send-success', 'Hartelijk dank, het formulier is succesvol verzonden.');
+l::set('sendform-send-error', 'Er is een fout bij het verzenden van het formulier opgetreden:');
+l::set('sendform-filled-potty', 'Er is een veld ingevuld wat leeg moet blijven. Mocht u geen Spam-Bot zijn, probeer het opnieuw zonder dat veld in te vullen.');
+l::set('sendform-no-email', 'E-Mail is niet ingevuld.');
