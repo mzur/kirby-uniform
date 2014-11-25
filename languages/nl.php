5b65b8f10a9f6681f1d86637dda5df83c2268e7d
Jöran Aerns <joranovski@gmail.com>
2014-11-25 20:16:29 +0100

    Added ducth translation
    Signed-off-by: Jöran Aerns <joranovski@gmail.com>

--- languages/nl.php
+++ languages/nl.php
@@ -6,5 +6,5 @@ l::set('sendform-send-error', 'Er is een fout bij het verzenden van het formulie
 l::set('sendform-filled-potty', 'Er is een veld ingevuld wat leeg moet blijven. Mocht u geen Spam-Bot zijn, probeer het opnieuw zonder dat veld in te vullen.');
 
 // TRANSLATION NEEDED
-l::set('sendform-fields-required', '');
-l::set('sendform-fields-not-valid', '');
+l::set('sendform-fields-required', 'Vul alstublieft alle verplichte velden in.');
+l::set('sendform-fields-not-valid', 'Sommige velden bevatten geen geldige informatie.');
