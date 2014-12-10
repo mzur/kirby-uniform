3c970a6821640b511b34c04f5ebcff4f769ea90e
Martin Zurowietz <martin@zurowietz.de>
2014-12-10 17:07:49 +0100

    Added an option to send copies of the form data to the sender of the form or to other email addresses.

--- languages/de.php
+++ languages/de.php
@@ -6,3 +6,4 @@ l::set('sendform-send-error', 'Es ist ein Fehler beim Senden aufgetreten:');
 l::set('sendform-filled-potty', 'Es wurde das Feld ausgefüllt, das leer bleiben sollte. Falls Sie kein Spam-Bot sind, versuchen Sie es bitte erneut ohne das Feld auszufüllen.');
 l::set('sendform-fields-required', 'Bitte füllen Sie alle benötigten Felder aus.');
 l::set('sendform-fields-not-valid', 'Einige Felder enthalten ungültige Daten.');
+l::set('sendform-email-copy', 'Kopie:');
--- languages/en.php
+++ languages/en.php
@@ -6,3 +6,4 @@ l::set('sendform-send-error', 'There was an error sending the form:');
 l::set('sendform-filled-potty', 'The form field that is supposed to be empty was filled. In case you are not a spam-bot, please try again leaving the field blank.');
 l::set('sendform-fields-required', 'Please fill in all required fields.');
 l::set('sendform-fields-not-valid', 'Some fields do not contain valid data.');
+l::set('sendform-email-copy', 'Copy:');
