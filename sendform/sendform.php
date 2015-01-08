1f143eddccfa072aa48eaa0cf40db846b4952aeb
Martin Zurowietz <martin@zurowietz.de>
2015-01-08 17:51:05 +0100

    Implemented cross platform directory separators. Closes #10.

--- sendform/sendform.php
+++ sendform/sendform.php
@@ -3,7 +3,9 @@
 /**
  * A simple Kirby 2 plugin to handle sending contact forms by e-mail.
  */
-if(!class_exists('SendForm')) require_once('lib/SendForm.php');
+if(!class_exists('SendForm')) {
+	require_once(__DIR__ . DS . 'lib' . DS . 'SendForm.php');
+}
 
 function sendform($id, $recipient, $options = array()) {
 	return new SendForm($id, $recipient, $options);
