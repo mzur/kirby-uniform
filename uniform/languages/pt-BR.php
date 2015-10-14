db6f0bc29730f5e570f03b25d48bd3c297cc8898
Rhawbert Costa <rhawbert@gmail.com>
2015-10-14 14:44:03 -0300

    Brazilian portuguese translation

---
+++ uniform/languages/pt-BR.php
@@ -0,0 +1,23 @@
+<?php
+
+l::set('uniform-filled-potty', 'O campo que deveria estar vazio foi preenchido. Caso você não seja um bot de spam, por favor tente novamente deixando o campo vazio.');
+l::set('uniform-fields-required', 'Por favor preencha todos campos requeridos.');
+l::set('uniform-fields-not-valid', 'Alguns campos não contém dados válidos.');
+
+l::set('uniform-email-subject', 'Mensagem do formulário web');
+l::set('uniform-email-success', 'Obrigado, o formulário foi enviado com sucesso.');
+l::set('uniform-email-error', 'Ocorreu um erro ao enviar o formulário:');
+l::set('uniform-email-copy', 'Copie:');
+
+l::set('uniform-calc-plus', 'mais');
+
+l::set('uniform-log-success', 'Entrada de log criada com sucesso.');
+l::set('uniform-log-error', 'Ocorreu um erro ao gravar o arquivo de log.');
+
+l::set('uniform-login-error', 'Nome de usuário ou senha incorreto.');
+l::set('uniform-login-success', 'Logado com sucesso.');
+
+l::set('uniform-webhook-success', 'Chamada webhook executada com sucesso.');
+l::set('uniform-webhook-error', 'Ocorreu um erro ao chamar a webhook: ');
+
+l::set('uniform-email-select-error', 'Recipiente inválido.');
