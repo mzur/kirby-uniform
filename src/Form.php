3ca7883f5e66f5d2ae21a175ee5191ac5cfd8f31
Martin Zurowietz <martin@zurowietz.de>
2016-12-10 17:41:00 +0100

    Init Uniform v3 experiment

---
+++ src/Form.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Uniform;
+
+use Jevets\Kirby\Form as BaseForm;
+
+/**
+*
+*/
+class Form extends BaseForm
+{
+
+    function __construct($data = [])
+    {
+        parent::__construct($data);
+    }
+}
--- uniform.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-require_once __DIR__.DS.'uniform'.DS.'uniform.php';
