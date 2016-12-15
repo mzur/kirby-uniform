42820493b38be4993a2bbf9374c21dbd27421ba7
Martin Zurowietz <martin@zurowietz.de>
2016-12-15 16:20:52 +0100

    Fix tests for PHP 5.4, remove HHVM

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,6 @@
 
 namespace Uniform\Tests;
 
-// Dirty hack to run tests even if s::start() of the Kirby Toolkit is called
-// see: http://stackoverflow.com/a/4059399/1796523
-ob_start();
-
 class TestCase extends \PHPUnit_Framework_TestCase
 {
     /**
