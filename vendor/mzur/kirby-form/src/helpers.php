a90da92e1942038024d61ad82dc9ca46cba4d9a1
Martin Zurowietz <martin@zurowietz.de>
2017-08-20 12:18:02 +0200

    Update kirby-form to v1.3.0

--- vendor/mzur/kirby-form/src/helpers.php
+++ vendor/mzur/kirby-form/src/helpers.php
@@ -22,3 +22,41 @@ if (!function_exists('csrf_field')) {
         return '<input type="hidden" name="'.Form::CSRF_FIELD.'" value="'.($t ?: $token).'">';
     }
 }
+
+if (class_exists('v')) {
+    // Extended validation rules for file uploads.
+    v::$validators['file'] = function ($value) {
+        return is_array($value) && array_key_exists('tmp_name', $value) && is_uploaded_file($value['tmp_name']);
+    };
+
+    v::$validators['filesize'] = function ($value, $size) {
+        // $size is in kb and $value['size'] is in byte, so multiply by 1000
+        return is_array($value) && array_key_exists('size', $value) && $value['size'] <= $size * 1000;
+    };
+
+    v::$validators['mime'] = function ($value, $allowed) {
+        if (is_string($value)) {
+          $name = $value;
+        } elseif (is_array($value) && array_key_exists('tmp_name', $value)) {
+          // This is for uploaded files from $_FILES
+          $name = $value['tmp_name'];
+        }
+        if (isset($name)) {
+          return in_array(f::mime($name), $allowed);
+        }
+        return false;
+    };
+
+    v::$validators['image'] = function ($value) {
+        if (is_string($value)) {
+          $name = $value;
+        } elseif (is_array($value) && array_key_exists('tmp_name', $value)) {
+          // This is for uploaded files from $_FILES
+          $name = $value['tmp_name'];
+        }
+        if (isset($name)) {
+          return f::type($name) === 'image';
+        }
+        return false;
+    };
+}
