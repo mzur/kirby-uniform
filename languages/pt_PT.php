76ea7ff4ad153162b4750e8546c3508f8154328a
Martin Zurowietz <martin@zurowietz.de>
2018-08-21 08:20:28 +0200

    Add Portugese language files
    Backport of #157

---
+++ languages/pt_PT.php
@@ -0,0 +1,17 @@
+<?php
+
+l::set([
+    'uniform-filled-potty' => 'O campo de formulário que deveria estar vazio foi preenchido. Caso você não seja um robô de SPAM, por favor tente novamente deixando o campo em branco.',
+    'uniform-calc-incorrect' => 'Por favor resolva o problema aritmético.',
+    'uniform-email-subject' => 'Mensagem do formulário online',
+    'uniform-email-error' => 'Houve um erro ao enviar o formulário',
+    'uniform-email-copy' => 'Cópia:',
+    'uniform-calc-plus' => 'mais',
+    'uniform-log-error' => 'Houve um erro ao escrever o ficheiro de registro.',
+    'uniform-login-error' => 'Usuário ou senha inválido.',
+    'uniform-webhook-error' => 'Houve um erro ao chamar o webhook: ',
+    'uniform-email-select-error' => 'Destinatário inválido.',
+    'uniform-upload-mkdir-fail' => 'Não foi possível criar a pasta de destino.',
+    'uniform-upload-exists' => 'O ficheiro já existe.',
+    'uniform-upload-failed' => 'O ficheiro não pode ser enviado.',
+]);
