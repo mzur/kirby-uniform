81e8166caef0be74996ae097d08cc2e262b32ffd
Martin Zurowietz <martin@zurowietz.de>
2023-10-13 19:59:21 +0200

    Fix bundled vendor files

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -6,16 +6,7 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    '6e3fae29631ef280660b3cdad06f25a8' => $vendorDir . '/symfony/deprecation-contracts/function.php',
-    '320cde22f66dd4f5d3fd621d3e88b98f' => $vendorDir . '/symfony/polyfill-ctype/bootstrap.php',
-    'e69f7f6ee287b969198c3c9d6777bd38' => $vendorDir . '/symfony/polyfill-intl-normalizer/bootstrap.php',
-    '6124b4c8570aa390c21fafd04a26c69f' => $vendorDir . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
     'e862d9fce5eaa28c798b151ac7fc8bc6' => $vendorDir . '/mzur/kirby-flash/src/helpers.php',
-    'f598d06aa772fa33d905e87be6398fb1' => $vendorDir . '/symfony/polyfill-intl-idn/bootstrap.php',
-    '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => $vendorDir . '/symfony/polyfill-mbstring/bootstrap.php',
-    'f864ae44e8154e5ff6f4eec32f46d37f' => $vendorDir . '/getkirby/cms/config/setup.php',
-    '87988fc7b1c1f093da22a1a3de972f3a' => $vendorDir . '/getkirby/cms/config/helpers.php',
     '316f57472eecfe71b6733d9d2acbcae9' => $vendorDir . '/mzur/kirby-form/src/helpers.php',
-    'ec07570ca5a812141189b1fa81503674' => $vendorDir . '/phpunit/phpunit/src/Framework/Assert/Functions.php',
     '1f7ddc2baab73fdd75a4663f7a275bb1' => $baseDir . '/src/helpers.php',
 );
